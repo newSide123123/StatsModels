aa59909791a7a2a793d63f32e5cd592f750fda64
newSide123123 <newSide01901@tutamail.com>
2024-07-31 14:16:48 +0300

    Fix issues with user authentication

---
+++ Square Every Digit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace Square_Every_Digit
+{
+    internal class Program
+    {
+        public static int SquareDigits(int n)
+        {
+            StringBuilder result = new StringBuilder();
+
+            foreach (char c in n.ToString())
+            {
+                result.Append((int)char.GetNumericValue(c) * (int)char.GetNumericValue(c));
+            }
+
+            return Convert.ToInt32(result.ToString());
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SquareDigits(9119));
+        }
+    }
+}
